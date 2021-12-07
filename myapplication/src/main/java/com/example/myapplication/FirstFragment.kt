5276157de43160ab8fd9348cfcb093c9064bb2d5
whiteboolean@gmail.com <whiteboolean@gmail.com>
2021-12-07 23:43:09 +0800

    IPC机制 Messenger 互传消息案例完善

--- myapplication/src/main/java/com/example/myapplication/FirstFragment.kt
+++ myapplication/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -4,10 +4,7 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.os.Bundle
-import android.os.IBinder
-import android.os.Message
-import android.os.Messenger
+import android.os.*
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -30,7 +27,18 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    private var messenger: Messenger? = null
+    private var mService: Messenger? = null
+    private val messageHandler: Handler = Handler(Looper.getMainLooper()) { msg ->
+        when (msg.what) {
+            2 -> {
+                Log.i(TAG, "receive msg from Server:${msg.data.getString("reply")}")
+//                    mService = Messenger(service)
+            }
+        }
+        false
+    }
+
+    private val mGetReplyMessenger: Messenger = Messenger(messageHandler)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,13 +50,22 @@ class FirstFragment : Fragment() {
 
     }
 
+
     private val connection: ServiceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            messenger = Messenger(service).apply {
-                send(Message.obtain(null, 1).also {
-                    Bundle().apply {
-                        putString("123", "123")
-                        it.data = this
+//            mService = Messenger(service).also { it ->
+//                it.send(Message.obtain(null, 1).also {
+//                    it.data = Bundle().apply {
+//                        putString("2324", "234")
+//                    }
+//                })
+//            }
+
+            mService = Messenger(service).also {
+                it.send(Message.obtain(null, 2).apply {
+                    data = Bundle().apply {
+                        putString("msg", "Hello ,this is client")
+                        replyTo = mGetReplyMessenger
                     }
                 })
             }
@@ -68,15 +85,16 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val intent = Intent(context, MessengerService::class.java)
+        context?.bindService(intent, connection, Context.BIND_AUTO_CREATE)
         binding.buttonFirst.setOnClickListener {
-            val intent = Intent(context, MessengerService::class.java)
-            context?.bindService(intent, connection, Context.BIND_AUTO_CREATE)
+
 //            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
 
 
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- myapplication/src/main/java/com/example/myapplication/art_dev_chapter2/MessengerService.java
+++ myapplication/src/main/java/com/example/myapplication/art_dev_chapter2/MessengerService.java
@@ -2,11 +2,13 @@ package com.example.myapplication.art_dev_chapter2;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -32,6 +34,19 @@ public class MessengerService extends Service {
             switch (msg.what){
                 case 1:
                     Log.e(TAG,"哈哈我接收到了数据");
+                    break;
+                case 2:
+                    Log.i(TAG,"receive msg from Client:"+msg.getData().getString("msg"));
+                    Messenger client = msg.replyTo;
+                    Message replyMessage = Message.obtain(null,2);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("reply","嗯，你的消息我已经收到，稍后会回复你。");
+                    replyMessage.setData(bundle);
+                    try {
+                        client.send(replyMessage);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     super.handleMessage(msg);
